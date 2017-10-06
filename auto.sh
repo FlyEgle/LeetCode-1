f2303b8f605af9a9adb719b23d51f193a3f16a15
m75n <mfisher@foxmail.com>
2017-10-06 20:19:01 +0800

    Add auto.sh

---
+++ auto.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+basepath=$(cd `dirname $0`; pwd)
+
+function show_problem {
+    cd $basepath/code
+
+    status=`git status | grep ".cpp" | sed -e 's/^[ \t]*//g'`
+
+    if [ -z "$status" ]
+    then
+        problemNo=`cat ../README.md | grep "\d*/\d*" | awk '{print $2}'`
+        problemNo=$[${problemNo%/*}+1]
+        leetcode show ${problemNo} -gxl cpp
+        problem=`ls | grep ${problemNo}.*.cpp`
+        vim ${problem}
+    else
+        problem=${status}
+        vim ${problem}
+    fi
+}
+
+function do_test {
+    cd $basepath/code
+
+    problem=`git status | grep ".cpp" | sed -e 's/^[ \t]*//g'`
+
+    if [ -z "$problem" ]
+    then
+        echo "Nothing for test."
+        return
+    fi
+
+    if [ -z "$2" ]
+    then
+        leetcode test ${problem}
+    else
+        leetcode test ${problem} -t $2
+    fi
+}
+
+function submit_problem {
+    cd $basepath/code
+
+    problem=`git status | grep ".cpp" | sed -e 's/^[ \t]*//g'`
+
+    if [ -z "$problem" ]
+    then
+        echo "Nothing for submit."
+    else
+        leetcode submit ${problem}
+    fi
+}
+
+function git_commit {
+    cd $basepath
+
+    str=`git status | grep ".cpp"`
+    str=${str#*/}
+    target=${str%%.*}
+    sedflag='s/'$[${target}-1]'\//'${target}'\//g'
+
+    sed -i ${sedflag} ./README.md
+
+    gitflag=`cat ./README.md | grep "\d*/\d*" | awk '{print $2}'`
+
+    git add --all
+    git commit -m ${gitflag}
+}
+
+function show_help {
+    cat << "EOF"
+SYNOPSIS
+    auto.sh [OPTION]
+
+DESCRIPTION
+    [none]  show problem
+    -t      do test
+    -s      submit
+    -c      git commit
+    -h      show_help
+EOF
+}
+
+if [ -z "$1" ]
+then
+    show_problem
+fi
+
+while [ -n "$1" ]
+do
+    case "$1" in
+        -t) do_test ;;
+        -s) submit_problem ;;
+        -c) git_commit ;;
+        -h) show_help ;;
+
+        *) show_help ;;
+    esac
+    shift
+done
